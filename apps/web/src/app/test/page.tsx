74792010141517a3e0908d5c976c9a72b9d0683a
Gabriel <gwmorata@gmail.com>
2026-09-23 00:15:18 -0300

    test: simple test page

---
+++ apps/web/src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Test Page</h1>
+      <p>If you can see this, the frontend is working!</p>
+      <p>Try accessing <code>/api/test</code> directly</p>
+    </div>
+  );
+}
